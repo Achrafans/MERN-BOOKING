8c1bf7c9e809cf30da6c49962287986c90b48626
Achrafans <122817393+Achrafans@users.noreply.github.com>
2023-06-10 16:00:53 +0100

    Mern-A

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,61 @@
+import "./login.scss"
+import axios from "axios";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { Navigate, useNavigate } from "react-router-dom";
+import MyImage from "../../images/png-clipart-desktop-wallpaper-removebg-preview.png";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("api/auth/login", credentials);
+      console.log(res);
+      if (res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        navigate("/");
+      } else {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { message: "You are not allowed!" },
+        });
+      }
+      
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
+    
+
+
+    return(
+    <div className="login" >
+        <div className="lContainer">
+          <img className="lImage" src={MyImage} alt="login.png" /><br/>
+          <h2><b>Welcome To As-Booking</b></h2>
+            <input type="text" placeholder="Username" id="username" onChange={handleChange} className="lInput" />
+            <input type="password" placeholder="Password" id="password" onChange={handleChange} className="lInput" />
+            <br></br>
+            <button disabled={loading} onClick={handleClick} className="lButton" >Login</button>
+            {error && <span>{error.message}</span>}
+        </div>
+
+        </div>
+    )
+};
+
+export default Login;
